c915e774dbc96d153e3b7d30737ac32d8d63bb52
renaegina <renaegina25778@gmail.com>
2024-02-06 22:40:11 +0700

    Aset Kecamatan

---
+++ views/about_tampil.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/logo.png"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI MANAJEMEN ASET <br> KECAMATAN Kita Bersama</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            <strong>Hak Akses</strong><br>
+                            <strong>1.Admin</strong>          Melakukan Pengolahan Data Dalam Sistem(Data Asset,Pajak dan User) dan Mencetak Laporan.<br>
+                            <strong>2.Camat/Pimpinan</strong> Melihat Data Dalam Sistem(Data Asset dan Pajak) Melihat dan Mencetak Laporan.<br>
+                            <strong>3.Masyarakat</strong>     Melihat Data Dalam Sistem(Data Asset dan Pajak) dan Hanya Dapat Melihat Data.<br><br>
+                            <strong>Tugas Kelompok Matakuliah Sistem Informasi Manajeman Atas Nama:</strong><br>
+                            <strong>Jessica Sianturi              NIM:212406025</strong><br>
+                            <strong>Rena Egina Br Pandia           NIM :212406015</strong><br>
+                            <strong>Cindya Novira Bangun           NIM : 212406025</strong><br>
+                            <strong>Monika Simbolon                NIM : 212406035</strong><br>
+                        
+                           
+                            <br>
+                            </p>
+                            <br><br><br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
